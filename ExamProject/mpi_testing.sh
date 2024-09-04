fabccede8756f1c2223f2d8013c0a33546f34bc4
francescoHubris <francesco.beno00@gmail.com>
2024-09-04 17:12:44 +0200

    final commit

--- ExamProject/mpi_testing.sh
+++ ExamProject/mpi_testing.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 #SBATCH -p normal
-#SBATCH --nodes=4 
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
 #SBATCH -o ./%j-mpitest.log
 #SBATCH -e ./%j-mpitest.err
 #SBATCH -t 00:50:00
 
 echo "Test executed on: $SLURM_JOB_NODELIST"
-make mpi
-echo "Executed of $1 with $3 process"
-make run_average_mpi FILE="$1.o" ARGS="$2 $3 $4"
+make $1
+echo "Executed of $1 with $SLURM_NTASKS process"
+make run_average_mpi FILE="$1.o" ARGS="$2 $3"
 echo "done"
